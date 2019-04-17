54f013767de71b7fd532e13cf8711343b548c302
Andrés Bayón <andres.bayon@anfix.com>
2019-04-17 12:13:01 +0200

    Soporte a CSS con PostCSS, precss y autoprefixer

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,5 @@
 import module from './_module';
+import './styles.css';
 
 window.onload = () => {
   console.log('Hola mundo');
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
     filename: 'scripts.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        { loader: 'css-loader', options: { importLoaders: 1 } },
+        'postcss-loader'
+      ],
+    }]
+  }
 };
